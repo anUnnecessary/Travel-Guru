33e83ddbd478584359fe40264f7e0241cf3eee3e
Abdullah Raihan Shamil <121636713+anUnnecessary@users.noreply.github.com>
2023-05-01 11:55:07 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Travel Guru 
+
+
+### Live preview -> <a href="https://travel-guru-09.web.app/">Travel Guru</a>
+
+
+
+
+
+
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCPlcnPktdWo_6BO_6WuImbyDsSYvtbd1g",
+  authDomain: "travel-guru-de159.firebaseapp.com",
+  projectId: "travel-guru-de159",
+  storageBucket: "travel-guru-de159.appspot.com",
+  messagingSenderId: "120633602131",
+  appId: "1:120633602131:web:64b83516de324eb93616a6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
